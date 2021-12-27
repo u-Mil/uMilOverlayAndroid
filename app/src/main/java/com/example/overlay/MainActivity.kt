d0d93e6497ad06e181f85750911f8225f0ee80fb
Mil <umil-3-@naver.com>
2021-12-27 17:47:15 +0900

    add Notification (Switch)

--- app/src/main/java/com/example/overlay/MainActivity.kt
+++ app/src/main/java/com/example/overlay/MainActivity.kt
@@ -9,15 +9,30 @@ import android.os.Bundle
 import android.provider.Settings
 import android.view.View
 import android.widget.Button
+import android.widget.Switch
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.NotificationManagerCompat
+
+import android.app.NotificationManager
+
+import android.app.NotificationChannel
+
+import android.content.Context
+import android.widget.CompoundButton
+
+import androidx.core.app.NotificationCompat
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btn : Button
+    lateinit var switch1 : Switch
     lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +41,16 @@ class MainActivity : AppCompatActivity() {
         checkPermissionForOverLay()
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        removeNotification()
+    }
+
     private fun init(){
         btn = findViewById(R.id.btn1);
-
+        switch1 = findViewById(R.id.switch1)
         btn.setOnClickListener(btnListener)
-
+        switch1.setOnCheckedChangeListener(switchListener)
 
         /*
          * startActivity(Intent intent) : new Activity
@@ -42,12 +62,10 @@ class MainActivity : AppCompatActivity() {
             ActivityResultCallback<ActivityResult>(){ result ->
                 // ? - nullable
                 if(result.resultCode == RESULT_CANCELED){
-                    alertDialogForPermission()
+                    alertDialogForOverlayPermission()
                 }
             })
     }
-
-
 
     /*
     * . (Dot)
@@ -58,6 +76,19 @@ class MainActivity : AppCompatActivity() {
     private var btnListener = View.OnClickListener { v ->
         if(v.id == R.id.btn1){
 
+        }
+    }
+
+    private var switchListener = CompoundButton.OnCheckedChangeListener{
+        // v - CompoundButton switch
+        // c - boolean check
+        v, c ->
+        if(v.id == R.id.switch1){
+            if(c){
+                createNotification()
+            }else{
+                removeNotification()
+            }
         }
     }
     fun checkPermissionForOverLay(){
@@ -73,7 +104,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun alertDialogForPermission(){
+    fun alertDialogForOverlayPermission(){
         var builder = AlertDialog.Builder(this)
         builder.setTitle("권한 요청").setMessage("앱을 사용하려면 '다른 앱 위에 표시(오버레이)' 권한이 필요합니다")
         builder.setPositiveButton("취소"){
@@ -88,5 +119,23 @@ class MainActivity : AppCompatActivity() {
         alertDialog.show()
     }
 
+    private fun createNotification() {
+        val builder = NotificationCompat.Builder(this, "default")
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+        builder.setContentTitle("Title")
+        builder.setContentText("content text")
+        builder.setAutoCancel(false)
+        builder.setOngoing(true)
+        val notificationManager =
+            this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(NotificationChannel("default","default", NotificationManager.IMPORTANCE_DEFAULT))
+        }
 
+        notificationManager.notify(1, builder.build())
+    }
+
+    private fun removeNotification() {
+        NotificationManagerCompat.from(this).cancel(1)
+    }
 }
