1a02009c7ca575acff21dc73deba3878507a1e57
Mil <umil-3-@naver.com>
2022-01-03 14:05:27 +0900

    fix.......

--- app/src/main/java/com/example/overlay/aService.kt
+++ app/src/main/java/com/example/overlay/aService.kt
@@ -4,6 +4,7 @@ import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.graphics.PixelFormat
+import android.os.Build
 import android.os.IBinder
 import android.view.*
 import android.view.WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
@@ -25,24 +26,20 @@ class aService : Service() {
     }
 
     private fun init(){
-        wm = getSystemService(WINDOW_SERVICE) as WindowManager
-
         var params = WindowManager.LayoutParams(
-            //width, height
-            100, 100,
-            // xpos, ypos
-            20, 20,
-            // _type
+            // SDK_INT >= O
             TYPE_APPLICATION_OVERLAY,
             // _flags
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE and
-                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL and
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
                 WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
             // _format
             // TRANSLUCENT,TRANSPARENT : 투명한
             PixelFormat.TRANSLUCENT
         )
-
+        params.width = 300
+        params.height = 300
+        params.gravity = Gravity.LEFT or Gravity.CENTER_VERTICAL
         // inflate layout_aservice.xml
         var inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         v = inflater.inflate(R.layout.layout_aservice, null)
@@ -50,6 +47,7 @@ class aService : Service() {
         var img1: ImageView = v.findViewById(R.id.img1_aserv)
         btn1.setOnClickListener(btnClicklistener)
 
+        wm = getSystemService(WINDOW_SERVICE) as WindowManager
         wm.addView(v, params)
     }
 
