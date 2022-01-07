b48f012c3944334d4706df2c3e479d291eb17a48
Mil <umil-3-@naver.com>
2022-01-07 13:37:24 +0900

    saparate initial method

--- app/src/main/java/com/example/overlay/aService.kt
+++ app/src/main/java/com/example/overlay/aService.kt
@@ -4,17 +4,24 @@ import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.graphics.PixelFormat
+import android.opengl.Visibility
 import android.os.Build
 import android.os.IBinder
 import android.view.*
 import android.view.WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.RelativeLayout
 
 class aService : Service() {
 
-    lateinit var wm:WindowManager
-    lateinit var v: View
+    lateinit var wm : WindowManager
+    lateinit var v : View
+    lateinit var rel_v : RelativeLayout
+    lateinit var params : WindowManager.LayoutParams
+    lateinit var inflater : LayoutInflater
+    private var x_val = 0.0f
+    private var y_val = 0.0f
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
@@ -25,43 +32,50 @@ class aService : Service() {
         init()
     }
 
-    private fun init(){
-        var params = WindowManager.LayoutParams(
-            // SDK_INT >= O
-            TYPE_APPLICATION_OVERLAY,
-            // _flags
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
-                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
-                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
-            // _format
-            // TRANSLUCENT,TRANSPARENT : 투명한
-            PixelFormat.TRANSLUCENT
-        )
-        params.width = 300
-        params.height = 300
-        params.gravity = Gravity.LEFT or Gravity.CENTER_VERTICAL
-        // inflate layout_aservice.xml
-        var inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        v = inflater.inflate(R.layout.layout_aservice, null)
-        v.setOnTouchListener(viewTouchListener)
-        var btn1: Button = v.findViewById(R.id.btn1_aserv)
-        var img1: ImageView = v.findViewById(R.id.img1_aserv)
-        btn1.setOnClickListener(btnClicklistener)
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
 
+    private fun init(){
+        setLayoutParamsToInit()
+        setLayoutInflater()
+        initInflatedViews()
         wm = getSystemService(WINDOW_SERVICE) as WindowManager
         wm.addView(v, params)
     }
 
     private var btnClicklistener = View.OnClickListener { v ->
-        if(v.id == R.id.btn1_aserv){
-            // do something....
-            wm.removeView(v)
-        }
+
     }
 
+    private fun setLayoutParamsToInit(){
+        params = WindowManager.LayoutParams(
+            // SDK_INT >= O
+            TYPE_APPLICATION_OVERLAY,
+            // _flags
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
+                    WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+            // _format
+            // TRANSLUCENT,TRANSPARENT : 투명한
+            PixelFormat.TRANSLUCENT
+        )
+    }
 
-    private var x_val = 0.0f
-    private var y_val = 0.0f
+    private fun setLayoutInflater(){
+        // inflate layout_aservice.xml
+        inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    }
+
+    private fun initInflatedViews(){
+        v = inflater.inflate(R.layout.layout_aservice, null)
+        var img1: ImageView = v.findViewById(R.id.img_aserv)
+        rel_v = v.findViewById(R.id.layout_aserv)
+        rel_v.setOnTouchListener(viewTouchListener)
+        img1.setImageResource(R.drawable.ic_launcher_foreground)
+        img1.visibility = View.VISIBLE
+    }
+
     private var viewTouchListener = View.OnTouchListener { v, event ->
         if(v.id == R.id.layout_aserv){
             when(event.action){
